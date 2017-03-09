820961d8e70c1c38f50206de24835320bcc33b8e
Patrick Daniel <patcdaniel@gmail.com>
2017-03-09 12:28:27 -0800

    Initial commit

---
+++ get_weather.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+
+url = "http://pubdata.mlml.calstate.edu/weather/recent.html"
+content = urlopen(url).read()
+soup = BeautifulSoup(content, 'html.parser')
+out = [tr.find('td').text for tr in soup.findAll('tr')]
+data = out[1]
+data = data.split('\n')
+data = [l for l in data if l != '']
+data = [l.strip('> ') for l in data]
+for i,l in enumerate(data):
+    if l.lower().find('wind speed') != -1:
+        windSpeed = float((re.search('Wind Speed:(\d+\.\d+) m/s.+',l).group(1)))
+    elif l.lower().find('wind direction') != -1:
+        windDir = int((re.search('Wind Direction:(\d+).+',l).group(1)))
+    elif l.lower().find('air temperature') != -1:
+        airTemp = float((re.search('Air Temperature:(\d+\.\d+).+',l).group(1)))
+        
